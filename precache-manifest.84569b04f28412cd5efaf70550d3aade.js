9fbf77ec93b2845cc59c2cd6a4b07664453458fe
abel <abelcheruvathoor@gmail.com>
2020-04-22 15:04:34 +0530

    Updates

---
+++ precache-manifest.84569b04f28412cd5efaf70550d3aade.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bb1c58f8dc1e3a37b6666a0b6b7395",
+    "url": "/react-countdown/index.html"
+  },
+  {
+    "revision": "d5ddbc546fbe84d033fa",
+    "url": "/react-countdown/static/css/main.6e80f785.chunk.css"
+  },
+  {
+    "revision": "5bbd99f3859de3591b98",
+    "url": "/react-countdown/static/js/2.01c53736.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-countdown/static/js/2.01c53736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ddbc546fbe84d033fa",
+    "url": "/react-countdown/static/js/main.012c6b96.chunk.js"
+  },
+  {
+    "revision": "96450643edede3f2ce3a",
+    "url": "/react-countdown/static/js/runtime-main.b05d7378.js"
+  },
+  {
+    "revision": "d2847b87c84dcbd53bc12cda56796ee3",
+    "url": "/react-countdown/static/media/Fontastique.d2847b87.woff"
+  },
+  {
+    "revision": "ed7d9d2d3213eecb0bc45b35ce02888f",
+    "url": "/react-countdown/static/media/circuit-board.ed7d9d2d.svg"
+  },
+  {
+    "revision": "c509a4c46008514b5a5084d2596c99e4",
+    "url": "/react-countdown/static/media/facebook.c509a4c4.svg"
+  },
+  {
+    "revision": "453685fbdc2f9491f26dd5ded0863c10",
+    "url": "/react-countdown/static/media/insta.453685fb.svg"
+  },
+  {
+    "revision": "505b9496b9d93ed68514bf26c62b2bf7",
+    "url": "/react-countdown/static/media/tathva.505b9496.svg"
+  },
+  {
+    "revision": "e5bf5a0cff4386700bc59167f9815a76",
+    "url": "/react-countdown/static/media/youtube.e5bf5a0c.svg"
+  }
+]);
